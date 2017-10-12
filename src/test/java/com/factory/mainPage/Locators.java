d5772da1ff9aa9326ce27e228878c564097f9b71
Yuriy12102017 <y.makitra@gmail.com>
2017-10-12 19:30:24 +0300

    Fixed project structure

---
+++ src/test/java/com/factory/mainPage/Locators.java
@@ -0,0 +1,10 @@
+package com.factory.mainPage;
+
+public class Locators {
+
+    public static String url = "http://www.ia.ca";
+    static String engLanguageBtn = "//*[@id=\"topLangMenuItem\"]/span";
+    static String loans = "//span[contains(.,'Loans')]";
+    static String mortgages = "//a[contains(.,'Mortgages')]";
+
+}
